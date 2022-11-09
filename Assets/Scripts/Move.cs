67649c9b74d8caa3a667a2d3a7467a4868226287
Alexandre Amiel <sachaamm@gmail.com>
2022-11-09 19:04:44 +0100

    SampleScene

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,10 +1,13 @@
+using System;
 using DefaultNamespace;
 using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    public float MoveSpeed = 1f;
+    
     void Update()
     {
-        transform.Translate(MasterManager.TranslateManager.TranslateDirection * Time.deltaTime);
+        transform.Translate(MasterManager.TranslateManager.TranslateDirection * (Time.deltaTime * MoveSpeed));
     }
 }
